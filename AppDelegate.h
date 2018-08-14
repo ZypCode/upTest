d5d5fd3ecbd74e02d0f22e8565f111b57b162b11
zhaiyupeng <zhaiyupeng@baidu.com>
2018-08-14 16:04:50 +0800

    add 3

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacAptAgent
+//
+//  Created by Zheng,Jie(SMD) on 2017/6/5.
+//  Copyright © 2017年 Zheng,Jie(SMD). All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
